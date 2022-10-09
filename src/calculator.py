341d6bfed16822326ed727b17c53bff915d1ea2b
salil-7295 <salil7295@gmail.com>
2022-10-09 20:40:57 +0200

    Fastapi App Containerized

---
+++ src/calculator.py
@@ -0,0 +1,31 @@
+"""
+The Calculator class which facilitates the calculation methods
+"""
+
+
+def calculator(method: str, value1: int, value2: int):
+    """
+
+    :param method:
+    :param value1:
+    :param value2:
+    :return:
+    """
+    if method == "Addition":
+        return value1 + value2
+
+    elif method == "Subtraction":
+        if value2 < value1:
+            return value1 - value2
+        else:
+            return value2 - value1
+
+    elif method == "Multiplication":
+        return value1 * value2
+
+    elif method == "Division":
+        return value1 / value2
+
+
+if __name__ == "__main__":
+    calculator("Addition", 10, 20)
---
+++ src/fast_app.py
@@ -0,0 +1,24 @@
+"""
+The Fast API module for the calculator
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from src.calculator import calculator
+
+
+class Input(BaseModel):
+    method: str
+    val1: int
+    val2: int
+
+
+app = FastAPI()
+
+
+@app.post("/calculate")
+def operation(user_input: Input):
+    return calculator(user_input.method, user_input.val1, user_input.val2)
+
+
+
